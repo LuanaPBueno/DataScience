ab3c4ce979068bd08cfc9358e505adfde0a50791
LuanaPBueno <luanabuenorj@gmail.com>
2024-10-10 18:56:09 -0300

    adding everything

---
+++ New/New/NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewApp.swift
+//  New
+//
+//  Created by Luana Bueno on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() //só para testes
+        }
+    }
+}
---
+++ New/NewTests/NewTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewTests.swift
+//  NewTests
+//
+//  Created by Luana Bueno on 10/10/24.
+//
+
+import Testing
+@testable import New
+
+struct NewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
